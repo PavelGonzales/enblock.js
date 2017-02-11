ab0a57af19837d73418ad0a41870fcac72abcad8
Pavel Gonzales <develop.gonzales@gmail.com>
2017-02-11 19:57:58 +0300

    Добавил обертку для всех внутренних блоков

--- src/index.js
+++ src/index.js
@@ -6,16 +6,21 @@ export default (elem) => {
   elems.forEach((item, i) => {
     data.push({
       name: 'block-' + (i + 1),
-      height: item.offsetHeight
+      height: item.offsetHeight,
+      top: item.offsetTop
     });
   });
+  window.data = data;
 
+  // console.log(data);
+  wrap(elem);
   detectScrollDirection();
+  detectBlockInSight(data);
 };
 
 function detectScrollDirection() {
   let oldValue = 0;
-  let direction;
+  let direction = false; 
   window.onscroll = () => {
     let newValue = window.pageYOffset || document.documentElement.scrollTop;
     if (newValue > oldValue) {
@@ -25,7 +30,31 @@ function detectScrollDirection() {
     }
     oldValue = newValue;
 
-    console.log(direction);
     return direction; 
   };
+};
+
+function detectBlockInSight(data) {
+  window.onscroll = () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const windowHeight = window.innerHeight;
+    const halfScreen = windowHeight / 2;
+    const scrollBottom = scrollTop + windowHeight;
+
+    // console.log('top => ', scrollTop);
+    // console.log('bottom => ', scrollBottom);
+
+    if ( scrollTop < data[3].top && scrollBottom >= data[3].top + data[3].height ) {
+      console.log(`${data[3].name} полностью в зоне видимости`);
+    }
+  }
+};
+
+function wrap(elem) {
+  const wrap = document.createElement('div');
+  const content = elem.innerHTML;
+  wrap.className = 'enblock-wrap';
+  elem.innerHTML = '';
+  elem.appendChild(wrap);
+  wrap.innerHTML = content;
 };
